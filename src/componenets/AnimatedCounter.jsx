929b68752060828dc543c4fb055b183c078fe5b2
sunit-03 <sunitmohan030303@gmail.com>
2025-08-02 20:42:23 +0530

    new commit

---
+++ src/componenets/AnimatedCounter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { counterItems } from "../constants";
+
+const AnimatedCounter = () => {
+  return (
+    <div id="counter" className="padding-x-lg xl:mt-0 mt-32">
+      <div className="mx-auto grid-3-cols">
+        <div>
+          {counterItems.map((item) => {
+            <div
+              key={counterItems.label}
+              className="counter-number text-white text-5xl font-bold mb-2"
+            >
+              0 {item.suffix}
+            </div>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnimatedCounter;
--- src/constants/index.js
+++ src/constants/index.js
@@ -22,17 +22,16 @@ const words = [
   { text: "Concepts", imgPath: "/images/concepts.svg" },
   { text: "Designs", imgPath: "/images/designs.svg" },
   { text: "Code", imgPath: "/images/code.svg" },
-  { text: "Ideas", imgPath: "/images/ideas.svg" },
-  { text: "Concepts", imgPath: "/images/concepts.svg" },
-  { text: "Designs", imgPath: "/images/designs.svg" },
-  { text: "Code", imgPath: "/images/code.svg" },
+  //   { text: "Ideas", imgPath: "/images/ideas.svg" },
+  //   { text: "Concepts", imgPath: "/images/concepts.svg" },
+  //   { text: "Designs", imgPath: "/images/designs.svg" },
+  //   { text: "Code", imgPath: "/images/code.svg" },
 ];
 
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 108, suffix: "+", label: "Completed Projects" },
-  { value: 90, suffix: "%", label: "Client Retention Rate" },
+  { value: 10, suffix: "+", label: "Months of Experience" },
+  { value: 50, suffix: "+", label: "Satisfied Clients" },
+  { value: 7, suffix: "+", label: "Completed Projects" },
 ];
 
 const logoIconsList = [
@@ -147,7 +146,8 @@ const techStackIcons = [
 
 const expCards = [
   {
-    review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+    review:
+      "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
     imgPath: "/images/exp1.png",
     logoPath: "/images/logo1.png",
     title: "Frontend Developer",
@@ -159,7 +159,8 @@ const expCards = [
     ],
   },
   {
-    review: "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+    review:
+      "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
     imgPath: "/images/exp2.png",
     logoPath: "/images/logo2.png",
     title: "Full Stack Developer",
@@ -171,7 +172,8 @@ const expCards = [
     ],
   },
   {
-    review: "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+    review:
+      "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
     imgPath: "/images/exp3.png",
     logoPath: "/images/logo3.png",
     title: "React Native Developer",
